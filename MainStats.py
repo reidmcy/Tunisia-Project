cf8947dbf5256df468fa0337434451886847229e
Reid <reid@reidmcy.com>
2015-03-26 15:02:29 -0400

    Added basic files

--- MainStats.py
+++ MainStats.py
@@ -1,13 +1,9 @@
 import CoathMaker
 import CoOrgMaker
 import CoCountyMaker
+import papersParse
+import networkx as nx
+
 
 if __name__ == "__main__":
-    plst = getfiles()
-    mlst = bin(pslt)
-    coathlst = coauth(mlst)
-    coContry = coCont(mlst)
-    coOrg = coOg(mlst)
-    for stast on newtwokx:
-        run
-        goto 1
+    inputPapersLst = papersParse.getfiles(isiSuffix)
