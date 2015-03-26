33685eab4448bb5f0f9ad1ac4b5bbf3b95b666d8
kousu <nick@kousu.ca>
2015-03-26 17:30:11 -0400

    First draft of the by-month stats-pumper

--- MainStats.py
+++ MainStats.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import print_function
 from __future__ import division
 
@@ -22,29 +21,42 @@ NETWORKS = [
 
 STATS = [
         nx.density,
-        nx.triangles
+        nx.triangles,
+        # add more stats methods here
+        # (you might need to write lambdas to wrap the arguments appropriately)
         #TODO: per-node stats. do we average them? take distributions?
         ]
 
-import pandas
+#import pandas
 
 if __name__ == "__main__":
     # load alllll the papers
     # as a 'table': a list of dictionaries
     papers = []
-    #sys.argv[1:]
-    for fname in getfiles():
-        papers.extend(isiParse(fname))
+    #
+    for fname in sys.argv[1:]:
+        papers.extend(isiParser(fname))
         
     # bin by month
-    monthly_networks = groupby(plst, lambda p: parse_month(p['PD'])
-    
+    def reformat_crufty_date(paper):
+        if 'PD' in paper and 'PY' in paper:
+            year = paper['PY']
+            year = year[0] #because stupidness
+            
+            month = paper['PD']
+            month = month[0] #because stupidness
+            return parse_year(year), parse_month(month)
+        else:
+            return None
+    monthly_networks = groupby(papers, reformat_crufty_date)
+    monthly_networks = dict(monthly_networks)
     
     for op in NETWORKS:
-        print(op.__name__)
+        print()
         
         # rewrite the 'table' (for each month) as a nx object
         networks = {m: op(n) for m, n in monthly_networks.items()}
+        import IPython; IPython.embed()
         
         # order by month
         months = sorted(networks.keys()) #XXX this sorted() is duplicated
@@ -54,4 +66,10 @@ if __name__ == "__main__":
         # one per desired statistic
         series = {stat.__name__: [stat(n) for m,n in networks] for stat in STATS}
         
-        print(series)
+        # TODO: put into a pandas.DataFrame, then:
+        # pandas.write_csv(series, op.__name__+".csv") -or-
+        # pandas.plot()
+        # in lieu of that:
+        
+        print(months) #<- keys
+        print(series) #<- values
